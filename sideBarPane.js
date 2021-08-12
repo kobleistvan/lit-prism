e20807e564670183e05b4843a86fcb1f38322e8f
Köble István <kobleistvan@gmail.com>
2021-08-12 19:28:27 +0300

    Sidebar now automatically updates periodically, and keeps the scroll position in the same place. Also keeps the expandable properties expanded, if there are no changes made to any of the properties.

--- sideBarPane.js
+++ sideBarPane.js
@@ -1,11 +1,28 @@
 const panels = chrome && chrome.devtools && chrome.devtools.panels;
-const bkg = chrome.extension.getBackgroundPage();
 
-const getPropsForSidebar = () => {
+const computeComponentProperties = () => {
     if ($0) {
         if ($0.__data__ || $0.__data) {
             // Polymer element
-            return Object.assign({}, $0.__data__, $0.__data);
+
+            /**
+             * Safely handles circular references. If the current iterated value is a non-null object, check if the prototype is
+             * different than the default Object.prototype. Discard if it's the same (we don't need them). Cache the objects in the local
+             * cache variable, and check for object duplicates, so we avoid cyclic references, which break the JSON compliancy.
+             */
+            const safeStringify = (obj, indent = 2) => {
+                let cache = [];
+
+                const retVal = JSON.stringify(obj, (key, value) =>
+                    typeof value === "object" && value !== null ? Object.getPrototypeOf(value) !== Object.prototype || cache.includes(value) ? undefined : cache.push(value) && value : value,
+                    indent);
+                return retVal;
+            };
+
+            const data = Object.assign({}, $0.__data, $0.__data__);
+
+            // Need to parse it back to a JSON object
+            return JSON.parse(safeStringify(data));
         } else {
             // Lit element
             const tagName = $0.localName;
@@ -20,23 +37,47 @@ const getPropsForSidebar = () => {
                 });
                 return result;
             } else {
-                return { error: 'Not a registered webComponent.' };
+                // Neither a Polymer nor a Lit element
+                return { message: 'The selected element is neither a Lit nor a Polymer webcomponent.' };
             }
         }
     }
 };
 
+
+let cachedResult = {};
+let rerenderData = true;
+
 const createSidebarPaneCallback = (sidebar) => {
 
     // Update element properties
     const updateElementProperties = () => {
-        sidebar.setExpression("(" + getPropsForSidebar + ")()");
+        chrome.devtools.inspectedWindow.eval("(" + computeComponentProperties + ")()",
+            (result, isException) => {
+                if (isException) {
+                    if (JSON.stringify(cachedResult) !== JSON.stringify(isException)) {
+                        rerender = true;
+                    }
+                    cachedResult = isException;
+                } else {
+                    if (JSON.stringify(cachedResult) !== JSON.stringify(result)) {
+                        rerender = true;
+                    }
+                    cachedResult = result;
+                }
+
+                if (rerender) {
+                    sidebar.setObject(cachedResult);
+                    rerender = false;
+                }
+            }
+        );
     }
 
     panels.elements.onSelectionChanged.addListener(updateElementProperties);
 
     // Update data periodically
-    // setInterval(updateElementProperties, 1000);
+    setInterval(updateElementProperties, 250);
 };
 
 panels && panels.elements.createSidebarPane("Lit Prism", createSidebarPaneCallback);
